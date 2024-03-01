b0ac9f62c7d5c69cbda6a6634b5d41d4c8ee177b
NoirFreedom <noirfreedom.xxx@gmail.com>
2024-03-01 23:22:10 +0900

    VideoRepository functions name RENAMED

--- tiktokApp_mock/lib/features/videos/view_models/timeline_view_model.dart
+++ tiktokApp_mock/lib/features/videos/view_models/timeline_view_model.dart
@@ -8,7 +8,7 @@ class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
   late final VideoRepository _videoRepository;
   List<VideoModel> _list = [];
 
-  Future<List<VideoModel>> _fetVideos({int? lastItemCreatedAt}) async {
+  Future<List<VideoModel>> _fetchVideos({int? lastItemCreatedAt}) async {
     final result = await _videoRepository.fetchVideos(
         lastItemCreatedAt: lastItemCreatedAt);
     final videos = result.docs
@@ -19,17 +19,18 @@ class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
   @override
   FutureOr<List<VideoModel>> build() async {
     _videoRepository = ref.read(videoRepo);
-    _list = await _fetVideos(lastItemCreatedAt: null);
+    _list = await _fetchVideos(lastItemCreatedAt: null);
     return _list;
   }
 
   Future<void> fetchNextPage() async {
-    final nextPage = await _fetVideos(lastItemCreatedAt: _list.last.createdAt);
+    final nextPage =
+        await _fetchVideos(lastItemCreatedAt: _list.last.createdAt);
     state = AsyncValue.data([..._list, ...nextPage]);
   }
 
   Future<void> refresh() async {
-    final videos = await _fetVideos(lastItemCreatedAt: null);
+    final videos = await _fetchVideos(lastItemCreatedAt: null);
     _list = videos;
     state = AsyncValue.data(videos);
   }
