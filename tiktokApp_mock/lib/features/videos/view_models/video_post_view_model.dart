9ac8b2aba286c0ee6821303e2b097ae6e0f3bf0c
NoirFreedom <noirfreedom.xxx@gmail.com>
2024-03-06 21:01:12 +0900

    Refactor video post view model to improve like functionality

--- tiktokApp_mock/lib/features/videos/view_models/video_post_view_model.dart
+++ tiktokApp_mock/lib/features/videos/view_models/video_post_view_model.dart
@@ -8,8 +8,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class VideoPostViewModel extends FamilyAsyncNotifier<VideoPostStatus, String> {
   late final VideoRepository _videoRepository;
   late final _videoId;
-  bool _isLiked = false; //! UI 업데이트를 위해 좋아요 상태를 false로 초기화
-  int _likeCounts = 0; //! UI 업데이트를 위해 좋아요 수를 0으로 초기화
 
   @override
   FutureOr<VideoPostStatus> build(String args) async {
@@ -18,41 +16,33 @@ class VideoPostViewModel extends FamilyAsyncNotifier<VideoPostStatus, String> {
     final user = ref.read(authRepo).user;
     final isLiked = await ref.read(videoRepo).isLikedVideo(_videoId, user!.uid);
     final likesCount = await ref.read(videoRepo).fetchLikesCount(_videoId);
+    print("isLiked: $isLiked");
+    print("likesCount: $likesCount");
 
     state = AsyncValue.data(
         VideoPostStatus(isLiked: isLiked, likesCount: likesCount));
     return state.value!;
   }
 
-  Future<void> toggleLikeVideo() async {
+  Future<int> toggleLikeVideo() async {
     final user = ref.read(authRepo).user;
-    if (user != null) {
-      await _videoRepository.toggleLikeVideo(_videoId, user.uid);
-    }
-    //! 즉각적인 UI 업데이트를 위해 좋아요 수를 증가 또는 감소 기능
-    final isLiked = await _videoRepository.toggleLikeVideo(_videoId, user!.uid);
-    _isLiked = !_isLiked;
-    if (_isLiked) {
-      _likeCounts += 1;
+    bool isLiked = await _videoRepository.toggleLikeVideo(_videoId, user!.uid);
+    int likesCount = await ref.read(videoRepo).fetchLikesCount(_videoId);
+
+    //! 수정 필요함(위 변수명과 아래 변수명이 일치하지 않으며, 토글기능을 수행중인지 확인 필요)
+    if (isLiked) {
+      final likesCount0 = likesCount + 1;
+      return likesCount0;
     } else {
       if (_likeCounts > 0) {
-        _likeCounts -= 1;
+        final likesCount0 = likesCount - 1;
+        return likesCount0;
       }
     }
 
     state = AsyncValue.data(
-        VideoPostStatus(isLiked: isLiked, likesCount: _likeCounts));
-  }
-
-  //! 사용 되지 않음
-  Future<bool> isLikedVideo(String videoId) async {
-    final user = ref.read(authRepo).user;
-    return _videoRepository.isLikedVideo(_videoId, user!.uid);
-  }
-
-  //! 사용 되지 않음
-  Future<int> fetchLikesCount(String videoId) async {
-    return _videoRepository.fetchLikesCount(videoId);
+        VideoPostStatus(isLiked: _isLiked, likesCount: _likeCounts));
+    return state.value!.likesCount;
   }
 }
 
