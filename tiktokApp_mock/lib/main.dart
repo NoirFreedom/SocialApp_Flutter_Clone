8a3bcbd8a4ff3a8ea2ff6d837d9f01ea7cf7b6f2
NoirFreedom <noirfreedom.xxx@gmail.com>
2024-04-03 21:46:18 +0900

    Refactor text theme in main.dart

--- tiktokApp_mock/lib/main.dart
+++ tiktokApp_mock/lib/main.dart
@@ -60,35 +60,6 @@ class SocialNetworkApp extends ConsumerWidget {
         useMaterial3: true,
         brightness: Brightness.light,
         textTheme: Typography.blackMountainView,
-        /* Material Type Scale
-          TextTheme(
-            displayLarge: GoogleFonts.openSans(
-                fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-            displayMedium: GoogleFonts.openSans(
-                fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-            displaySmall:
-                GoogleFonts.openSans(fontSize: 48, fontWeight: FontWeight.w400),
-            headlineMedium: GoogleFonts.openSans(
-                fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-            headlineSmall:
-                GoogleFonts.openSans(fontSize: 24, fontWeight: FontWeight.w400),
-            titleLarge: GoogleFonts.openSans(
-                fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-            titleMedium: GoogleFonts.openSans(
-                fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-            titleSmall: GoogleFonts.openSans(
-                fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-            bodyLarge: GoogleFonts.roboto(
-                fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-            bodyMedium: GoogleFonts.roboto(
-                fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-            labelLarge: GoogleFonts.roboto(
-                fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-            bodySmall: GoogleFonts.roboto(
-                fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-            labelSmall: GoogleFonts.roboto(
-                fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-          ),*/
         textSelectionTheme: const TextSelectionThemeData(
           cursorColor: Color(0xFFE9435A),
           selectionHandleColor: Color(0xFFE9435A),
