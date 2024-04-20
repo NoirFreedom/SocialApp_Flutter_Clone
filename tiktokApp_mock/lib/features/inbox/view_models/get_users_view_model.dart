375ad0e4761cf33ef5477c14f22d7fc25c006892
NoirFreedom <noirfreedom.xxx@gmail.com>
2024-04-20 21:56:22 +0900

    Refactor GetUsersViewModel to handle async loading and error states

--- tiktokApp_mock/lib/features/inbox/view_models/get_users_view_model.dart
+++ tiktokApp_mock/lib/features/inbox/view_models/get_users_view_model.dart
@@ -1,28 +1,29 @@
 import 'dart:async';
-
 import 'package:TikTok/features/inbox/repos/chatrooms_repo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GetUsersViewModel extends AsyncNotifier<void> {
+class GetUsersViewModel extends AsyncNotifier<List<Map<String, dynamic>>> {
   late final ChatRoomsRepository chatRoomsRepo;
 
   @override
-  FutureOr build() {
+  FutureOr<AsyncValue<List<Map<String, dynamic>>>> build() {
     chatRoomsRepo = ref.read(chatRoomsProvider);
+    //! 값 리턴해야함
   }
 
   Future<void> getUsers() async {
     state = const AsyncLoading();
-    state = await AsyncValue.guard(
-      () async {
-        final users = await chatRoomsRepo.getUsersInfo();
-        return users;
-      },
-    );
-    print(state);
+    try {
+      final users = await chatRoomsRepo.getUsersInfo();
+      state = AsyncValue.data(users);
+    } catch (e) {
+      state = AsyncValue.error(e, StackTrace.current);
+    }
+    print("state: $state");
   }
 }
 
-final getUsersProvider = AsyncNotifierProvider<GetUsersViewModel, void>(
+final getUsersProvider =
+    AsyncNotifierProvider<GetUsersViewModel, List<Map<String, dynamic>>>(
   () => GetUsersViewModel(),
 );
